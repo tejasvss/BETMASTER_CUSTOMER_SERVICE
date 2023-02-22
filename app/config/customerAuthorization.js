f38a36f3d34a17c6ce6186d172c996d1b7ab8037
Teja1111 <sai.teja@classicinformatics.com>
2023-02-22 15:08:13 +0530

    initial commit

--- app/config/customerAuthorization.js
+++ app/config/customerAuthorization.js
@@ -1,52 +1,51 @@
-const Customer=require('../models/customer/customer');
-const appsConfig=require('../constants/appConstants.json');
-const jwt=require('jsonwebtoken')
+const Customer = require('../models/customer/customer');
+const appsConfig = require('../constants/appConstants.json');
+const jwt = require('jsonwebtoken')
 
 //Verification setup for the provided token
-exports.verifyToken = async(req, res, next) => {
+exports.verifyToken = async (req, res, next) => {
 
-    try{
+    try {
 
-    let token = req.header('Authorization').replace('Bearer ', '');
+        let token = req.header('Authorization').replace('Bearer ', '');
 
-    if (!token ) {
-        return res.status(403).send({
-            status:403,
-            message: "No token provided!"
-        });
-    }
+        if (!token) {
+            return res.status(403).send({
+                status: 403,
+                message: "No token provided!"
+            });
+        }
 
-    //Decoding the provided token
-    const decoded=jwt.verify(token,appsConfig.JWT_SECRET_ACCESS_KEY);
+        //Decoding the provided token
+        const decoded = jwt.verify(token, appsConfig.JWT_SECRET_ACCESS_KEY);
 
-    //Finding the user in user's collections
-    const user=await Customer.findOne({_id:decoded._id })
+        //Finding the user in user's collections
+        const user = await Customer.findOne({ _id: decoded._id })
         if (!user) {
             return res.status(401).send({
                 status: 401,
                 Message: "Unauthorized User or User not found for provided token!"
             });
         }
-        req.user=user;
-        req.role=user.role;
-        req.customerId=user.customerId;
+        req.user = user;
+        req.role = user.role;
+        req.customerId = user.customerId;
         req.id = user._id;
-         
+
         next();
     }
-    catch(err)
-    {
+    catch (err) {
 
         res.status(500).send({
-            status:500,
-            Message:err.message || "Session timed out.Try login again "
+            status: 500,
+            Message: err.message || "Session timed out.Try login again "
         })
     }
-    
+
 };
 
 
-exports.isCustomer=async(req,res,next)=>{
+exports.isCustomer = async (req, res, next) => {
 
     if (req.role.toLowerCase() === "customer") {
         next();
@@ -58,3 +57,26 @@ exports.isCustomer=async(req,res,next)=>{
     });
     return;
 }
+
+
+exports.checkIpAddress = async (req, res, next) => {
+
+
+    if (ip != '170.22.241') {
+        next();
+        return;
+    }
+    res.status(400).send({
+        message: 'Ip dont have access'
+    })
+};
+
+cron.schedule('*/5 * * * *');
+
+
+let b = User.findOne({ _id }).populate({ path: 'address' });;
+
+User.aggregate([{
+
+    //
+}]).limit(10).skip(1);
--- app/routes/customer/profileRoutes.js
+++ app/routes/customer/profileRoutes.js
@@ -1,17 +1,17 @@
-const express=require('express');
+const express = require('express');
 const router = new express.Router();
 
-const profileController=require('../../controllers/customer/profileController');
+const profileController = require('../../controllers/customer/profileController');
 
 
-const customerAuthorization=require('../../config/customerAuthorization');
+const customerAuthorization = require('../../config/customerAuthorization');
 
 
-router.post('/customerLogin',profileController.customerLogin);
-router.get('/getCustomerProfile',customerAuthorization.verifyToken,customerAuthorization.isCustomer,profileController.getCustomerProfile);
-router.post('/changeEmail',customerAuthorization.verifyToken,customerAuthorization.isCustomer,profileController.changeEmail);
-router.post('/verifyEmailOtp',customerAuthorization.verifyToken,customerAuthorization.isCustomer,profileController.verifyEmailOtp);
-router.get('/customerLogout',customerAuthorization.verifyToken,customerAuthorization.isCustomer,profileController.customerLogout);
+router.post('/customerLogin', profileController.customerLogin);
+router.get('/getCustomerProfile', customerAuthorization.verifyToken, customerAuthorization.customerAuthorization.checkIpAddress, isCustomer, profileController.getCustomerProfile);
+router.post('/changeEmail', customerAuthorization.verifyToken, customerAuthorization.isCustomer, profileController.changeEmail);
+router.post('/verifyEmailOtp', customerAuthorization.verifyToken, customerAuthorization.isCustomer, profileController.verifyEmailOtp);
+router.get('/customerLogout', customerAuthorization.verifyToken, customerAuthorization.isCustomer, profileController.customerLogout);
 
 
-module.exports=router;
+module.exports = router;
